eaab6dab06d9431bdf58758d662b79af3c88330c
stojchevska <138624193+tamarastojchevska@users.noreply.github.com>
2025-07-16 16:42:35 +0200

    Add models, admin and forms

--- mostApp/admin.py
+++ mostApp/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from mostApp.models import *
+
 # Register your models here.
+
+class ProfilePostInline(admin.TabularInline):
+    model = Post
+    extra = 0
+
+class ProfileApplicationPostInline(admin.TabularInline):
+    model = ApplicationPost
+    extra = 0
+
+class ProfileCertificationInline(admin.TabularInline):
+    model = Certification
+    extra = 0
+
+
+class  ProfileAdmin(admin.ModelAdmin):
+    inlines = [ProfilePostInline, ProfileApplicationPostInline, ProfileCertificationInline]
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Post)
+admin.site.register(ApplicationPost)
+admin.site.register(Certification)
